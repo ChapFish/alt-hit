38c3c8215ce5514965e262d5fcfc74ee9bda0661
ChapFish <tech.octo@gmail.com>
2017-07-16 11:05:30 +0900

    Initial Commit

---
+++ alt/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  alt
+//
+//  Created by 岡田航汰 on 2017/07/16.
+//  Copyright © 2017年 Kota Okada. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
