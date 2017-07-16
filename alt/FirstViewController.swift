44a941dc5bf60ed2dc485f1c6f220a5eb8f60a98
ChapFish <tech.octo@gmail.com>
2017-07-17 02:21:51 +0900

    LectureCardTableViewCellの実装をひとまず完了。

--- alt/FirstViewController.swift
+++ alt/FirstViewController.swift
@@ -11,15 +11,17 @@ import UIKit
 //実験用定数
 let lectureName = ["経済学入門", "", "社会科学概論", "法学入門", "経済学入門"]
 let lectureRoom = ["1201", "", "21", "20", "401"]
+let lectureStatus = ["経済学部", "", "社会学部", "法学部", "経済学部"]
+let lectureFlag = [false, false, false, false, true]
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var CardTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        self.CardTableView.register(UINib(nibName:"LectureCardViewCell", bundle:nil), forCellReuseIdentifier: "LectureCardViewCellID")
+        self.CardTableView.register(UINib(nibName: "LectureCardTableViewCell", bundle:nil), forCellReuseIdentifier: "LectureCardViewCellID")
 
         //セルの高さを自動設定
         self.CardTableView.estimatedRowHeight = 200
@@ -34,5 +36,16 @@ class FirstViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func numberOfSections(in tableView:UITableView) -> Int{
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "LectureCardViewCellID", for: indexPath) as! LectureCardTableViewCell
+        cell.setLectureCard(lectureName: lectureName, lectureRoom: lectureRoom, lectureStatus: lectureStatus, lectureFlag: lectureFlag)
+        return cell
+    }
+    
 }
--- alt/LectureCardTableViewCell.swift
+++ alt/LectureCardTableViewCell.swift
@@ -10,8 +10,20 @@ import UIKit
 
 class LectureCardTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var LectureContenairView: UIView!
+    @IBOutlet weak var FirstLectureNameLabel: UILabel!
+    @IBOutlet weak var SecondLectureNameLabel: UILabel!
+    @IBOutlet weak var ThirdLectureNameLabel: UILabel!
+    @IBOutlet weak var FourthLectureNameLabel: UILabel!
+    @IBOutlet weak var FifthLectureNameLabel: UILabel!
+    @IBOutlet weak var FirstLectureRoomLabel: UILabel!
+    @IBOutlet weak var SecondLectureRoomLabel: UILabel!
+    @IBOutlet weak var ThirdLectureRoomLable: UILabel!
+    @IBOutlet weak var FourthLectureRoomLabel: UILabel!
+    @IBOutlet weak var FifthLectureRoomLabel: UILabel!
+    
     override func awakeFromNib() {
-        super.awakeFromNib()
+                super.awakeFromNib()
         // Initialization code
     }
 
@@ -20,5 +32,32 @@ class LectureCardTableViewCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
-    
+ 
+    func setLectureCard(lectureName:Array<String>, lectureRoom:Array<String>, lectureStatus:Array<String>, lectureFlag:Array<Bool>){
+    //カードのコンテンツを入力（入力内容はSetLectureCardItem.swiftで規定）
+        LectureContenairView.setLectureIndicatorColor(lectureStatus: lectureStatus)
+        FirstLectureNameLabel.setLectureName(lectureName: lectureName, lectureFlag: lectureFlag, lectureNameCount: 1)
+        SecondLectureNameLabel.setLectureName(lectureName: lectureName, lectureFlag: lectureFlag, lectureNameCount: 2)
+        ThirdLectureNameLabel.setLectureName(lectureName: lectureName, lectureFlag: lectureFlag, lectureNameCount: 3)
+        FourthLectureNameLabel.setLectureName(lectureName: lectureName, lectureFlag: lectureFlag, lectureNameCount: 4)
+        FifthLectureNameLabel.setLectureName(lectureName: lectureName, lectureFlag: lectureFlag, lectureNameCount: 5)
+        FirstLectureRoomLabel.setLectureRoom(lectureRoom: lectureRoom, lectureFlag: lectureFlag, lectureRoomCount: 1)
+        SecondLectureRoomLabel.setLectureRoom(lectureRoom: lectureRoom, lectureFlag: lectureFlag, lectureRoomCount: 2)
+        ThirdLectureRoomLable.setLectureRoom(lectureRoom: lectureRoom, lectureFlag: lectureFlag, lectureRoomCount: 3)
+        FourthLectureRoomLabel.setLectureRoom(lectureRoom: lectureRoom, lectureFlag: lectureFlag, lectureRoomCount: 4)
+        FifthLectureRoomLabel.setLectureRoom(lectureRoom: lectureRoom, lectureFlag: lectureFlag, lectureRoomCount: 5)
+    //カードの見た目を設定
+        self.backgroundColor = UIColor.clear
+        self.LectureContenairView.backgroundColor = UIColor.white
+        self.LectureContenairView.layer.shadowColor = UIColor.black.cgColor /* 影の色 */
+        self.LectureContenairView.layer.shadowOffset = CGSize(width:2, height:2) /* 影の大きさ */
+        self.LectureContenairView.layer.shadowOpacity = 0.3 /* 透明度 */
+        self.LectureContenairView.layer.shadowRadius = 1 /* 影の距離 */
+        self.layer.shadowPath = UIBezierPath(rect: self.bounds).cgPath
+        self.layer.shouldRasterize = true
+        self.layer.rasterizationScale = UIScreen.main.scale
+        self.LectureContenairView.layer.masksToBounds = false
+        
+    }
+
 }
